aa0a7b843f62163d694c836c2843dbffda989a45
Joan Trejos <joak99@javerianacali.edu.co>
2017-11-04 12:00:21 -0500

    calc

--- calc/gridsizer.h
+++ calc/gridsizer.h
@@ -14,6 +14,14 @@ public:
   void onCls (wxCommandEvent&);
   void onBt1 (wxCommandEvent&);
   void onBt2 (wxCommandEvent&);
+  void onBt3 (wxCommandEvent&);
+  void onBt4 (wxCommandEvent&);
+  void onBt5 (wxCommandEvent&);
+  void onBt6 (wxCommandEvent&);
+  void onBt7 (wxCommandEvent&);
+  void onBt8 (wxCommandEvent&);
+  void onBt9 (wxCommandEvent&);
+  void onBt0 (wxCommandEvent&);
   void onBtSum (wxCommandEvent&);
 
 private:
